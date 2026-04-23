0ac02df49018900883a18831ee618cbf39b8b96d
Carlos Taquez <carlostaquez232@gmail.com>
2026-04-23 09:27:00 -0500

    Agregar vistas y templates de libros

--- proyecto/proyecto/urls.py
+++ proyecto/proyecto/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from gestion import views as gestion_views
 
 urlpatterns = [
+    path('', gestion_views.home, name='home'),
     path('admin/', admin.site.urls),
     path('libros/', include('gestion.urls')),
 ]
